af7c007f709520f86bca7aef27909de2e54238d5
dariormt <47364660+dariormt@users.noreply.github.com>
2023-06-30 09:51:24 +0000

    2nd Day Final
    The final day of the project

--- .ccls-cache/@home@runner@2048-Terminal-Game/main.cpp
+++ .ccls-cache/@home@runner@2048-Terminal-Game/main.cpp
@@ -1,19 +1,48 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 
 // variables
-const int boardSize = 4;
-int score = 0;
+int boardSize = 4;      // if i do a vector of vectors, i can do this, yay!!
+                        //  size, but array of an array dosn't like it
+int startingPieces = 2; // Customize easyly
+int piecesToSpawn = 1;
 
-const std::string art[] = {
-    "\033[1;33m██████   ██████  ██   ██  █████  \n"
+int score = 0;
+int maxscore = 3950; // hard-coded highscore it works for now
+
+// bool retry = true; //fuck the compiler, why does an int work and a bool not
+
+int state = 1; // current game state 1 replay, 0 exit;
+
+const std::string art[] = {                         // art thingis
+    "\033[1;33m██████   ██████  ██   ██  █████  \n" // 0
     "     ██ ██  ████ ██   ██ ██   ██ \n"
     " █████  ██ ██ ██ ███████  █████  \n"
     "██      ████  ██      ██ ██   ██ \n"
     "███████  ██████       ██  █████  \033[0m\n"
-    "					By: Dariormt\n"};
+    "			By: Dario Rodriguez\n\n",
+    "__   __ _____  _   _     _    _  _____  _   _   _ \n" // 1
+    "\\ \\ / /|  _  || | | |   | |  | ||_   _|| \\ | | | |\n"
+    " \\ V / | | | || | | |   | |  | |  | |  |  \\| | | |\n"
+    "  \\ /  | | | || | | |   | |/\\| |  | |  | . ` | | |\n"
+    "  | |  \\ \\_/ /| |_| |   \\  /\\  / _| |_ | |\\  | |_|\n"
+    "  \\_/   \\___/  \\___/     \\/  \\/  \\___/ \\_| \\_/ (_)\n\n",
+    " ______     ______     __    __     ______        ______     __   __  "
+    " "
+    "______     ______    \n"
+    "/\\  ___\\   /\\  __ \\   /\\ '-./  \\   /\\  ___\\      /\\  __ \\   "
+    "/\\ " // 2
+    "\\ / /  /\\  ___\\   /\\  == \\   \n"
+    "\\ \\ \\__ \\  \\ \\  __ \\  \\ \\ \\-./\\ \\  \\ \\  __\\      \\ \\ "
+    "\\/\\ \\  \\ \\ \' /   \\ \\  __\\   \\ \\  __<   \n"
+    " \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_\\ \\ \\_\\  \\ \\_____\\     \\ "
+    "\\_____\\  \\ \\__|    \\ \\_____\\  \\ \\_\\ \\_\\ \n"
+    "  \\/_____/   \\/_/\\/_/   \\/_/  \\/_/   \\/_____/      \\/_____/   "
+    "\\/_/      \\/_____/   \\/_/ /_/ \n\n"};
 
-std::string ColorParser(int numb) { // parses color depending on num
+std::string ColorParser(
+    int numb) { // parses color depending on num using ascii color escapes
   switch (numb) {
   case 0:
     return "\033[1;30m0\033[0m";
@@ -52,25 +81,64 @@ std::string ColorParser(int numb) { // parses color depending on num
     return "\033[1;97m2048\033[0m";
     break;
   default:
-    return "!";
+    return "!"; // wtf
     break;
   }
 }
 
-void InitBoard(int board[boardSize][boardSize]) { // Board initialization
+bool IsFull(std::vector<std::vector<int>> &board) {
+  for (int i = 0; i < boardSize; i++) {
+    for (int j = 0; j < boardSize; j++) {
+      if (board[i][j] == 0) { // empty state
+        return false;
+      }
+    }
+  }
+  // if not empty
+  return true;
+}
+
+void SpawnTile(std::vector<std::vector<int>> &board, int ammount) {
+  if (IsFull(board)) { // if it's full we can't spawn a numb
+    return;
+  }
+  for (int i = 0; i < ammount; i++) { // does iterations for each piece
+    if (IsFull(board)) {              // if it's full we can't spawn a numb
+      return;
+    }
+
+    int num;
+    while (true) // keep repeating until we get an empty space
+    {
+      int row = rand() % boardSize; // Random pos
+      int col = rand() % boardSize;
+      if (rand() % 2 == 0) { // 50/50 prob to be 2 or 4
+        num = 2;
+      } else {
+        num = 4;
+      }
+
+      if (board[row][col] == 0) { // if the space is empty, set space and exit
+                                  // loop
+        board[row][col] = num;
+        break;
+      }
+    }
+  }
+}
+
+void InitBoard(std::vector<std::vector<int>> &board) { // Board initialization
 
   srand(time(NULL)); // setup random
 
+  board.resize(boardSize);
   for (int i = 0; i < boardSize; i++) {
+    board[i].resize(boardSize);
     for (int j = 0; j < boardSize; j++) {
       board[i][j] = 0; // initialize all of the array
     }
   }
-  int numSpawn1 =
-      rand() %
-      boardSize; // Choose a random spot on the 2D array to spawn first number
-  int numSpawn2 = rand() % boardSize;
-  board[numSpawn1][numSpawn2] = 2;
+  SpawnTile(board, startingPieces);
 }
 
 int SeeNumlenght(int numb) {
@@ -126,19 +194,49 @@ void DrawTile(int num, int pos) {
   }
 }
 
-void DrawGame(int board[boardSize][boardSize]) { // Board Drawing
+void DrawSeparation(int h) { // implements procedural box gen
+  switch (h) {
+  case -1:
+    std::cout << "┌";
+    for (int i = 0; i < boardSize - 1; i++) { // each column
+      std::cout << "───┬";
+    }
+    std::cout << "───┐\n";
+    break;
+  case 0:
+    std::cout << "├";
+    for (int i = 0; i < boardSize - 1; i++) { // each column
+      std::cout << "───┼";
+    }
+    std::cout << "───┤\n";
+    break;
+  case 1:
+    std::cout << "└";
+    for (int i = 0; i < boardSize - 1; i++) { // each column
+      std::cout << "───┴";
+    }
+    std::cout << "───┘\n";
+    break;
+  default:
+    std::cout << "┼───┼───┼───┼───┼\n"; // end line
+    break;
+  }
+}
+
+void DrawGame(std::vector<std::vector<int>> &board) { // Board Drawing
 
   // Clear the console
   std::cout << "\033[2J\033[1;1H";
   // Draw Cover
   std::cout << art[0];
 
-  for (int i = 0; i < boardSize; i++) {
+  std::cout << "Score: " << score << "  |  Highscore: " << maxscore << '\n';
 
+  for (int i = 0; i < boardSize; i++) {
     if (i == 0) { // create box initial separation
-      std::cout << "┌───┬───┬───┬───┐\n";
+      DrawSeparation(-1);
     } else { // create box separation between rows
-      std::cout << "├───┼───┼───┼───┤\n";
+      DrawSeparation(0);
     }
 
     for (int j = 0; j < boardSize; j++) {
@@ -148,24 +246,12 @@ void DrawGame(int board[boardSize][boardSize]) { // Board Drawing
     std::cout << '\n'; // Do new line
   }
 
-  std::cout << "└───┴───┴───┴───┘\n"; // close box enclosure
+  DrawSeparation(1);
 
   std::cout << std::endl; // End and flush
 }
 
-bool IsFull(int board[boardSize][boardSize]) {
-  for (int i = 0; i < boardSize; i++) {
-    for (int j = 0; j < boardSize; j++) {
-      if (board[i][j] == 0) { // empty state
-        return false;
-      }
-    }
-  }
-  // if not empty
-  return true;
-}
-
-bool IsGameOverState(int board[boardSize][boardSize]) {
+bool IsGameOverState(std::vector<std::vector<int>> &board) {
   for (int i = 0; i < boardSize; i++) {
     for (int j = 0; j < boardSize; j++) {
       if (board[i][j] == 0) { // check if has empty cells
@@ -184,7 +270,7 @@ bool IsGameOverState(int board[boardSize][boardSize]) {
   return true;
 }
 
-bool IsWinState(int board[boardSize][boardSize]) {
+bool IsWinState(std::vector<std::vector<int>> &board) {
   for (int i = 0; i < boardSize; i++) {
     for (int j = 0; j < boardSize; j++) {
       if (board[i][j] == 2048) { // if contains numb
@@ -196,31 +282,7 @@ bool IsWinState(int board[boardSize][boardSize]) {
   return false;
 }
 
-void SpawnTile(int board[boardSize][boardSize]) {
-  if (IsFull(board)) { // if it's full we can't spawn a numb, btw game over
-                       // state will be triggered
-    return;
-  }
-  int num;
-  while (true) // keep repeating until we get an empty space
-  {
-    int row = rand() % boardSize; // Random pos
-    int col = rand() % boardSize;
-    if (rand() % 2 == 0) { // 50/50 prob to be 2 or 4
-      num = 2;
-    } else {
-      num = 4;
-    }
-
-    if (board[row][col] == 0) { // if the space is empty, set space and exit
-                                // loop
-      board[row][col] = num;
-      break;
-    }
-  }
-}
-
-bool MoveUp(int board[boardSize][boardSize]) {
+bool MoveUp(std::vector<std::vector<int>> &board) {
   bool moved = false;
   for (int j = 0; j < boardSize; j++) {
     for (int i = 1; i < boardSize; i++) {
@@ -237,6 +299,7 @@ bool MoveUp(int board[boardSize][boardSize]) {
         if (k > 0 && board[k - 1][j] == board[k][j]) {
           board[k - 1][j] += board[k][j];
           board[k][j] = 0;
+          score += board[k - 1][j]; // sum score
           moved = true;
         }
       }
@@ -245,7 +308,7 @@ bool MoveUp(int board[boardSize][boardSize]) {
   return moved; // return if any moves has been performed
 }
 
-bool MoveDown(int board[boardSize][boardSize]) {
+bool MoveDown(std::vector<std::vector<int>> &board) {
   bool moved = false;
   for (int j = 0; j < boardSize; j++) {
     for (int i = boardSize - 2; i >= 0; i--) {
@@ -267,6 +330,7 @@ bool MoveDown(int board[boardSize][boardSize]) {
                                               // sum them
           board[k + 1][j] += board[k][j];
           board[k][j] = 0;
+          score += board[k + 1][j]; // sum score
           moved = true;
         }
       }
@@ -275,14 +339,76 @@ bool MoveDown(int board[boardSize][boardSize]) {
   return moved; // return if any moves has been performed
 }
 
-void DoInput(int board[boardSize][boardSize]) {
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(200));
+bool MoveLeft(std::vector<std::vector<int>> &board) {
   bool moved = false;
+  for (int i = 0; i < boardSize; i++) {
+    for (int j = 1; j < boardSize; j++) {
+      if (board[i][j] != 0) { // if cell is not empty
+        int k = j;
+        while (k > 0 && board[i][k - 1] == 0) { // moves each full
+                                                // cell until none of them
+                                                // can be moved further
+          board[i][k - 1] = board[i][k];
+          board[i][k] = 0;
+          k--;
+          moved = true;
+        }
+
+        if (k > 0 && board[i][k - 1] == board[i][k]) { // if next moving cell
+                                                       // is the same
+                                                       // as the current cell,
+                                                       // sum them
+          board[i][k - 1] += board[i][k];
+          board[i][k] = 0;
+          score += board[i][k - 1]; // sum score
+          moved = true;
+        }
+      }
+    }
+  }
+  return moved;
+}
+
+bool MoveRight(std::vector<std::vector<int>> &board) {
+  bool moved = false;
+  for (int i = 0; i < boardSize; i++) {
+    for (int j = boardSize - 2; j >= 0; j--) {
+      if (board[i][j] != 0) { // if cell is not empty
+        int k = j;
+        while (k < boardSize - 1 && board[i][k + 1] == 0) { // moves each full
+          // cell until none of them
+          // can be moved further
+          board[i][k + 1] = board[i][k];
+          board[i][k] = 0;
+          k++;
+          moved = true;
+        }
+        if (k < boardSize - 1 &&
+            board[i][k + 1] == board[i][k]) { // if next moving cell
+                                              // is the same
+                                              // as the current cell,
+                                              // sum them
+          board[i][k + 1] += board[i][k];
+          board[i][k] = 0;
+          score += board[i][k + 1]; // sum score
+          moved = true;
+        }
+      }
+    }
+  }
+  return moved;
+}
+
+void DoInput(std::vector<std::vector<int>> &board) {
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(150)); // wait
+
+  bool moved = false;
+
   char userMove;
   std::cout << "Enter move [W] -> UP [S] -> DOWN [A] -> LEFT [D] -> RIGHT : ";
   std::cin >> userMove;
-  userMove = toupper(userMove);
+  userMove = toupper(userMove); // to upper so it can be put both
 
   switch (userMove) {
   case 'W':
@@ -292,10 +418,10 @@ void DoInput(int board[boardSize][boardSize]) {
     moved = MoveDown(board);
     break;
   case 'A':
-
+    moved = MoveLeft(board);
     break;
   case 'D':
-
+    moved = MoveRight(board);
     break;
   default:
     std::cout << "\033[1;31mInvalid move (W,A,S,D)\033[0m\n";
@@ -307,16 +433,70 @@ void DoInput(int board[boardSize][boardSize]) {
   }
 
   if (moved) {
-    SpawnTile(board);
+    SpawnTile(board, piecesToSpawn);
     DrawGame(board);
   } else {
-    std::cout << "\033[1;31mNo valid moves left in this direction!\033[0m\n";
+    std::cout << "\033[1;31mNo valid moves left in this direction!\033[0m\n"; //
     return;
   }
 }
 
-int main() {
-  int board[boardSize][boardSize];
+void InitialSetup() { // unused
+  // Clear the console
+  std::cout << "\033[2J\033[1;1H";
+  // Draw Cover
+  std::cout << art[0];
+
+  char sel;
+  std::cout << "[1] -> Quick start [2] -> Custom game : ";
+  std::cin >> sel;
+  switch (sel) {
+  case '1':
+    return;
+    break;
+  case '2':
+    std::cout << "Board size, min 2 - max 20 (default 4): ";
+    std::cin >> boardSize;
+    if (boardSize > 20) {
+      boardSize = 20;
+    } else if (boardSize < 2) {
+      boardSize = 2;
+    }
+
+    std::cout << "Starting pieces, min 1 - max " << (boardSize * boardSize) - 1
+              << " (default 2): ";
+    std::cin >> startingPieces;
+    if (startingPieces > (boardSize * boardSize) - 5) {
+      startingPieces = (boardSize * boardSize) - 5;
+    } else if (startingPieces < 1) {
+      startingPieces = 1;
+    }
+
+    std::cout << "Pieces to spawn, min 1 - max " << (boardSize * boardSize) - 1
+              << " (default 1): ";
+    std::cin >> piecesToSpawn;
+    if (piecesToSpawn > (boardSize * boardSize) - 5) {
+      piecesToSpawn = (boardSize * boardSize) - 5;
+    } else if (piecesToSpawn < 1) {
+      piecesToSpawn = 1;
+    }
+    return;
+    break;
+  }
+}
+
+void CheckHighscore() { // simple thingi
+  if (score > maxscore) {
+    std::cout << "New highscore: " << score << '\n';
+    maxscore = score;
+    score = 0;
+  }
+}
+
+void MainGame() {
+  InitialSetup();
+
+  std::vector<std::vector<int>> board;
 
   InitBoard(board);
   DrawGame(board);
@@ -324,8 +504,52 @@ int main() {
   while (!IsGameOverState(board) && !IsWinState(board)) {
     DoInput(board);
   }
-  // finish movement
-  // win state
-  // lose state
-  // add scores
+
+  if (IsGameOverState(board)) {
+    std::cout << art[2];
+    CheckHighscore();
+
+    char retry;
+    std::cout << "Try again? (y|n): "; // try again
+    std::cin >> retry;
+    if (retry == 'y') {
+      state = 1;
+      return;
+    } else if (retry == 'n') {
+      state = 0;
+      return;
+    } else {
+      state = 1;
+      return;
+    }
+  } else if (IsWinState(board)) {
+    std::cout << art[1];
+    CheckHighscore();
+
+    char retry;
+    std::cout << "Try again? (y|n): "; // try again
+    std::cin >> retry;
+    if (retry == 'y') {
+      state = 1;
+      return;
+    } else if (retry == 'n') {
+      state = 0;
+      return;
+    } else {
+      state = 1;
+      return;
+    }
+  }
+  return;
+}
+
+int main() {
+  while (true) { // retry loop
+    if (state != 0) {
+      MainGame();
+    } else {
+      break;
+    }
+  }
+  return 0;
 }
